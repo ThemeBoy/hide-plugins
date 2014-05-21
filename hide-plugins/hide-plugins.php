ea40d1e0b12f297858bf25abaa99512b08019041
ThemeBoy <support@themeboy.com>
2014-05-21 18:48:36 +1000

    Add hide-plugins version 1.0.2

---
+++ hide-plugins/hide-plugins.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * @package Hide_Plugins
+ * @version 1.0.2
+ */
+/*
+Plugin Name: Hide Plugins
+Plugin URI: http://wordpress.org/plugins/hide-plugins/
+Description: Hide installed plugins from all other users.
+Author: ThemeBoy
+Version: 1.0.2
+Author URI: http://themeboy.com/
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Plugin setup
+ *
+ * @since 1.0
+*/
+class Hide_Plugins {
+
+	/**
+	 * @var string
+	 */
+	public $filename = 'hide-plugins/hide-plugins.php';
+
+	/**
+	 * Hide Plugins Constructor.
+	 * @access public
+	 */
+	public function __construct() {
+
+		// Activate plugin
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
+
+		// Define constants
+		$this->define_constants();
+
+		// Hooks
+		add_action( 'init', array( $this, 'init' ) );
+		add_filter( 'option_hide_plugins', array( $this, 'hide_self' ) );
+		add_filter( 'all_plugins', array( $this, 'prepare_items' ) );
+		add_filter( 'plugin_action_links', array( $this, 'action_links' ), 10, 4 );
+		add_action( 'admin_action_hide_plugin', array( $this, 'hide_action' ) );
+		add_action( 'admin_action_show_plugin', array( $this, 'show_action' ) );
+	}
+
+	/**
+	 * Define constants
+	*/
+	private function define_constants() {
+		if ( !defined( 'HIDE_PLUGINS_VERSION' ) )
+			define( 'HIDE_PLUGINS_VERSION', '1.0.2' );
+
+		if ( !defined( 'HIDE_PLUGINS_URL' ) )
+			define( 'HIDE_PLUGINS_URL', plugin_dir_url( __FILE__ ) );
+
+		if ( !defined( 'HIDE_PLUGINS_DIR' ) )
+			define( 'HIDE_PLUGINS_DIR', plugin_dir_path( __FILE__ ) );
+	}
+
+	/**
+	 * Install
+	 */
+	public function install() {
+		$current_user = wp_get_current_user();
+		update_option( 'hide_plugins_user', $current_user->ID );
+	}
+
+	/**
+	 * Init plugin when WordPress Initialises.
+	 */
+	public function init() {
+		// Set up localisation
+		$this->load_plugin_textdomain();
+	}
+
+	/**
+	 * Load Localisation files.
+	 *
+	 * Note: the first-loaded translation file overrides any following ones if the same translation is present
+	 */
+	public function load_plugin_textdomain() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'hide-plugins' );
+		
+		// Global + Frontend Locale
+		load_plugin_textdomain( 'hide-plugins', false, plugin_basename( dirname( __FILE__ ) . "/languages" ) );
+	}
+
+	/**
+	 * Hide this plugin from other users.
+	 */
+	public function hide_self( $option ) {
+		if ( ! is_array( $option ) ) {
+			$option = array();
+		}
+		if ( ! in_array( $this->filename, $option ) ) {
+			$option[] = $this->filename;
+		}
+		return $option;
+	}
+
+	/**
+	 * Prepare plugins.
+	 */
+	public function prepare_items( $plugins ) {
+		$user = get_option( 'hide_plugins_user' );
+		$current_user = wp_get_current_user();
+
+		if ( ! isset( $user ) || ! get_userdata( $user ) || ! isset( $current_user ) ) return $plugins;
+
+		$hidden = (array) array_unique( get_option( 'hide_plugins', array( $this->filename ) ) );
+		foreach ( $hidden as $filename ) {
+			if ( array_key_exists( $filename, $plugins ) ) {
+				if ( $user == $current_user->ID ) {
+					$plugins[ $filename ]['Name'] = '<em>' . $plugins[ $filename ]['Name'] . '</em>';
+				} else {
+					unset( $plugins[ $filename ] );
+				}
+			}
+		}
+		return $plugins;
+	}
+
+	/**
+	 * Add the action link.
+	 */
+	public function action_links( $actions, $plugin_file, $plugin_data, $context ) {
+		global $page, $s;
+		$user = get_option( 'hide_plugins_user' );
+		$current_user = wp_get_current_user();
+
+		if ( $user != $current_user->ID ) return $actions;
+
+		$hidden = (array) get_option( 'hide_plugins', array( $this->filename ) );
+		if ( $this->filename != $plugin_file ) {
+			if ( in_array( $plugin_file, $hidden ) ) {
+				$actions['show'] = '<a href="' . wp_nonce_url('plugins.php?action=show_plugin&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'show-plugin_' . $plugin_file) . '" title="' . esc_attr__('Show this plugin', 'show-plugins') . '" class="edit">' . __('Show', 'hide-plugins') . '</a>';
+			} else {
+				$actions['hide'] = '<a href="' . wp_nonce_url('plugins.php?action=hide_plugin&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'hide-plugin_' . $plugin_file) . '" title="' . esc_attr__('Hide this plugin', 'hide-plugins') . '" class="edit">' . __('Hide', 'hide-plugins') . '</a>';
+			}
+		}
+		return $actions;
+	}
+
+	/**
+	 * Hide a plugin action.
+	 */
+	public function hide_action() {
+		if ( empty( $_REQUEST['plugin'] ) ) {
+			wp_die(__( 'Plugin file does not exist.', 'hide-plugins' ));
+		}
+
+		// Get the filename
+		$filename = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+
+		// Hide the plugin
+		if ( ! empty( $filename ) ) {
+			$hidden = get_option( 'hide_plugins', array() );
+			if ( ! in_array( $filename, $hidden ) ) {
+				$hidden[] = $filename;
+			}
+			update_option( 'hide_plugins', $hidden );
+		} else {
+			wp_die(__( 'Plugin file does not exist.', 'hide-plugins' ) );
+		}
+	}
+
+	/**
+	 * Show a plugin action.
+	 */
+	public function show_action() {
+		if ( empty( $_REQUEST['plugin'] ) ) {
+			wp_die(__( 'Plugin file does not exist.', 'hide-plugins' ));
+		}
+
+		// Get the filename
+		$filename = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+
+		// Show the plugin
+		if ( ! empty( $filename ) ) {
+			$hidden = get_option( 'hide_plugins', array() );
+			if ( ( $key = array_search( $filename, $hidden ) ) !== false ) {
+				unset( $hidden[ $key ] );
+			}
+			update_option( 'hide_plugins', $hidden );
+		} else {
+			wp_die(__( 'Plugin file does not exist.', 'hide-plugins' ) );
+		}
+	}
+}
+
+new Hide_Plugins();
---
+++ hide-plugins/readme.txt
@@ -0,0 +1,76 @@
+=== Hide Plugins ===
+Contributors: ThemeBoy
+Tags: hide, hidden, plugins, admin, dashboard, secret, visibility, privacy, invisible, ninja
+Requires at least: 3.0
+Tested up to: 3.9.1
+Stable tag: 1.0.2
+License: GPLv2 or later
+
+Hide installed plugins from clients and other admin users.
+
+== Description ==
+
+Hide Plugins is a light-weight plugin that gives a single admin user the ability to hide plugins prevent them from being activated, deactivated, or deleted by clients and other users, including administrators. By activating Hide Plugins, you will be able to see all plugins and a toggle to hide each plugin from other users on the Plugins page. Hide Plugins will always remain hidden.
+
+Note that the dropdown on the Edit Plugins page will not be affected, since it does not have a filter to hook into. Hidden plugins will remain active, so traces of the plugin in areas other than the Plugins page (like options pages in the admin menu) will still be visible. If you also want to hide menus, we recommend using [Admin Menu Editor](https://wordpress.org/plugins/admin-menu-editor/).
+
+== Installation ==
+
+= Minimum Requirements =
+* WordPress 3.0 or greater
+* PHP version 5.2.4 or greater
+* MySQL version 5.0 or greater
+
+= Automatic Installation =
+
+Automatic installation is the easiest option as WordPress handles the file transfers itself and you don’t even need to leave your web browser. To do an automatic install of Hide Plugins, log in to your WordPress admin panel, navigate to the Plugins menu and click Add New.
+
+In the search field type "Hide Plugins" and click Search Plugins. Once you’ve found the plugin you can view details about it such as the point release, rating and description. Most importantly of course, you can install it by simply clicking Install Now. After clicking that link you will be asked if you’re sure you want to install the plugin. Click yes and WordPress will automatically complete the installation.
+
+= Manual Installation =
+
+The manual installation method involves downloading the plugin and uploading it to your webserver via your favorite FTP application.
+
+1. Download the plugin file to your computer and unzip it
+2. Using an FTP program, or your hosting control panel, upload the unzipped plugin folder to your WordPress installation’s wp-content/plugins/ directory.
+3. Activate the plugin from the Plugins menu within the WordPress admin.
+
+= Upgrading =
+
+Automatic updates should work like a charm; as always though, ensure you backup your site just in case.
+
+== Screenshots ==
+
+1. Plugins page as viewed by Hide Plugin's master admin user.
+2. Plugins page as viewed by another admin user.
+
+== Frequently Asked Questions ==
+
+= What does it hide? =
+
+Hide Plugins does one thing: it prevent plugins from appearing on the Plugins page in your WordPress dashboard for all other users. Any other areas of the dashboard that may be affected by hidden plugins, like settings pages and dashboard widgts, will not be hidden.
+
+= Which plugins can I hide? =
+
+Hide Plugins will always be hidden by default, when it is active. You will see new action links under every plugin on the Plugins page. Click "Hide" to hide a plugin from other users. Click "Show" to unhide the plugin, so that it is visible to everyone again. Hidden plugins will be displayed in italics.
+
+= Who does it hide plugins from? =
+
+Hide Plugins will hide the chosen plugins from all other users, including admin users. The only person who will be able to see hidden plugins is you, the master user.
+
+= How can I also hide admin menu links? =
+
+Install the [Admin Menu Editor](https://wordpress.org/plugins/admin-menu-editor/) plugin to re-order, hide, or rename menus, add custom menus and more.
+
+== Changelog ==
+
+= 1.0.2 =
+* Tweak - Display hidden plugin names in italics instead of appending " - Hidden" string.
+
+= 1.0.1 =
+* Localization - Japanese translations added.
+
+= 1.0 =
+* Feature - Save current user as Hide Plugins master on activation.
+* Feature - Label hidden plugins and display actions links to master user.
+* Feature - Hide selected plugins from all other users.
